1394bd2905694f2f7ddfc66dbe8459629f2c680c
nazon098 <huichanyang60@gmail.com>
2026-05-12 11:28:03 +0900

    Battle.py 업데이트

--- Battle.py
+++ Battle.py
@@ -2,3 +2,5 @@ import random
 Health_Mastery = 0
 Player_Health = 100 + Health_Mastery
 Room = [1,2,3,4,5,6,7,8,9,10]
+Monster = [1,2,3,4,5,6,7,8,9,10]
+Weapon = {'Wooden_Sword':2,'Stone_Sword':4,'Iron_Sword':6,'Diamond_Sword':9,'Legendary_Sword':15}
